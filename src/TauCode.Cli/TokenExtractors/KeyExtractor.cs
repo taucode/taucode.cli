78e79a981470ba9a125e3e5f0cb3e0ff1da338a9
Andrii Kovalenko <Andrii@invictusgurus.com>
2019-12-24 17:56:47 +0200

    Happy xmas!

---
+++ src/TauCode.Cli/TokenExtractors/KeyExtractor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using TauCode.Cli.Tokens;
+using TauCode.Parsing;
+using TauCode.Parsing.Lexing;
+
+namespace TauCode.Cli.TokenExtractors
+{
+    public class KeyExtractor : TokenExtractorBase
+    {
+        public KeyExtractor(ILexingEnvironment environment)
+            : base(environment, c => c == '-')
+        {
+        }
+
+        protected override IToken ProduceResult()
+        {
+            var str = this.ExtractResultString();
+            var token = new KeyToken(str);
+            return token;
+        }
+
+        protected override CharChallengeResult ChallengeCurrentChar()
+        {
+            var c = this.GetCurrentChar();
+            var pos = this.GetLocalPosition();
+
+            if (pos == 0)
+            {
+                return CharChallengeResult.Continue; // 0th char MUST have been accepted.
+            }
+
+            if (pos == 1)
+            {
+                if (c == '-' || LexingHelper.IsDigit(c) || LexingHelper.IsLatinLetter(c))
+                {
+                    return CharChallengeResult.Continue;
+                }
+
+                return CharChallengeResult.GiveUp;
+            }
+
+            if (c == '-')
+            {
+                throw new NotImplementedException();
+            }
+
+            if (LexingHelper.IsDigit(c) || LexingHelper.IsLatinLetter(c))
+            {
+                return CharChallengeResult.Continue;
+            }
+
+            // todo: test keys "-", "--", "---", "--fo-", "-fo-" etc.
+            if (this.Environment.IsSpace(c))
+            {
+                return CharChallengeResult.Finish;
+            }
+
+            return CharChallengeResult.GiveUp;
+        }
+
+        protected override CharChallengeResult ChallengeEnd()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/TauCode.Cli/TokenExtractors/PathExtractor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TauCode.Cli.Tokens;
+using TauCode.Extensions;
+using TauCode.Parsing;
+using TauCode.Parsing.Lexing;
+
+namespace TauCode.Cli.TokenExtractors
+{
+    public class PathExtractor : TokenExtractorBase
+    {
+        public PathExtractor(ILexingEnvironment environment)
+            : base(environment, IsPathFirstChar)
+        {
+        }
+
+        private static bool IsPathFirstChar(char c) =>
+            LexingHelper.IsDigit(c) ||
+            LexingHelper.IsLatinLetter(c) ||
+            c.IsIn('\\', '/', '.', '!', '~', '$', '%', '-', '+');
+
+
+        protected override IToken ProduceResult()
+        {
+            var str = this.ExtractResultString();
+            var token = new PathToken(str);
+            return token;
+
+        }
+
+        protected override CharChallengeResult ChallengeCurrentChar()
+        {
+            var c = this.GetCurrentChar();
+            var pos = this.GetLocalPosition();
+
+            if (pos == 0)
+            {
+                return CharChallengeResult.Continue; // 0th char MUST have been accepted.
+            }
+
+            if (IsPathFirstChar(c) || c == ':')
+            {
+                return CharChallengeResult.Continue;
+            }
+
+            if (this.Environment.IsSpace(c))
+            {
+                return CharChallengeResult.Finish;
+            }
+
+            return CharChallengeResult.GiveUp;
+        }
+
+        protected override CharChallengeResult ChallengeEnd()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
