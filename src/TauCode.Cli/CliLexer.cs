558abaf90342d03b208923183cd18df4ee989e23
Andrii Kovalenko <Andrii@invictusgurus.com>
2019-12-24 17:57:17 +0200

    Missed a file.

---
+++ src/TauCode.Cli/CliLexer.cs
@@ -0,0 +1,32 @@
+﻿using TauCode.Cli.TokenExtractors;
+using TauCode.Parsing.Lexing;
+using TauCode.Parsing.Lexing.StandardTokenExtractors;
+
+namespace TauCode.Cli
+{
+    public class CliLexer : LexerBase
+    {
+        protected override void InitTokenExtractors()
+        {
+            // integer
+            var integerExtractor = new IntegerExtractor(this.Environment);
+            this.AddTokenExtractor(integerExtractor);
+
+            // term
+            var termExtractor = new TermExtractor(this.Environment);
+            this.AddTokenExtractor(termExtractor);
+
+            // key
+            var keyExtractor = new KeyExtractor(this.Environment);
+            this.AddTokenExtractor(keyExtractor);
+
+            // string
+            var stringExtractor = new StringExtractor(this.Environment);
+            this.AddTokenExtractor(stringExtractor);
+
+            // path
+            var pathExtractor = new PathExtractor(this.Environment);
+            this.AddTokenExtractor(pathExtractor);
+        }
+    }
+}
