8f929461234f2e39286b397808f8c525f23b1205
Andrii Kovalenko <Andrii@invictusgurus.com>
2020-01-20 17:21:26 +0200

    Deleted some todos.

--- src/TauCode.Cli/Help/Tokens/WhiteSpaceToken.cs
+++ src/TauCode.Cli/Help/Tokens/WhiteSpaceToken.cs
@@ -3,8 +3,6 @@ using TauCode.Parsing.Tokens;
 
 namespace TauCode.Cli.Help.Tokens
 {
-    // todo: 'INoPayloadToken' in taucode.parsing.
-    // todo: 'bool AddNoPayloadTokens' in LexerBase
     public class WhiteSpaceToken : TokenBase
     {
         public WhiteSpaceToken(Position position, int consumedLength)
