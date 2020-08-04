ebe4a2afee60800132a6480aa419ab7126208dfb
Andrey Kovalenko <ak@taucode.com>
2020-08-04 17:54:37 +0300

    Switching to new scheme

--- dev-build-release.bat
+++ dev-build-release.bat
@@ -3,7 +3,7 @@ dotnet restore
 dotnet build --configuration Debug
 dotnet build --configuration Release
 
-dotnet test -c Debug .\tests\TauCode.Cli.Tests\TauCode.Cli.Tests.csproj
-dotnet test -c Release .\tests\TauCode.Cli.Tests\TauCode.Cli.Tests.csproj
+dotnet test -c Debug .\tests\TauCode.Cli.Tests.UnitTests\TauCode.Cli.Tests.UnitTests.csproj
+dotnet test -c Release .\tests\TauCode.Cli.Tests.UnitTests\TauCode.Cli.Tests.UnitTests.csproj
 
 nuget pack nuget\TauCode.Cli.nuspec
