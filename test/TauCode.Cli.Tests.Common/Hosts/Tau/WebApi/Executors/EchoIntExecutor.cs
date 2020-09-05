8e260d0f6ef52df0b048103fc0e09c2e636768b8
Andrey Kovalenko <ak@taucode.com>
2020-09-05 16:46:08 +0300

    Moving to new pipeline scheme.

---
+++ test/TauCode.Cli.Tests.Common/Hosts/Tau/WebApi/Executors/EchoIntExecutor.cs
@@ -0,0 +1,15 @@
+﻿using TauCode.Extensions;
+
+namespace TauCode.Cli.Tests.Common.Hosts.Tau.WebApi.Executors
+{
+    public class EchoIntExecutor : CommonExecutor
+    {
+        public EchoIntExecutor()
+            : base(
+                typeof(CqrsExecutor).Assembly.GetResourceText(".Tau.WebApi.EchoInt.lisp", true),
+                null, 
+                true)
+        {
+        }
+    }
+}
